e0b9a5886bcdafb27a2af0cae42f72ec6d5beda1
Rabindra Dhakal <contact@qaidvoid.dev>
2024-11-12 17:57:14 +0545

    ci(workflow): update release workflow

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::process::Command;
+
+fn main() {
+    if std::env::var("SOAR_NIGHTLY").is_ok() {
+        let commit_sha = Command::new("git")
+            .arg("rev-parse")
+            .arg("--short")
+            .arg("HEAD")
+            .output()
+            .expect("Failed to get git commit SHA")
+            .stdout;
+
+        let commit_sha = String::from_utf8(commit_sha).expect("Invalid UTF-8 output").trim().to_string();
+
+        println!("cargo:rerun-if-changed=build.rs");
+        println!("cargo:rustc-env=CARGO_PKG_VERSION=nightly-{}", commit_sha);
+    }
+}
